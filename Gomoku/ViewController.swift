a242a814d0721c9814f40acf69ab403b549f2287
Hirose.Yudai <hirose.yudai@moneyforward.co.jp>
2016-02-24 17:08:22 +0900

    Initial Commit

---
+++ Gomoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gomoku
+//
+//  Created by Hirose.Yudai on 2016/02/24.
+//  Copyright © 2016年 Hirose.Yudai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
